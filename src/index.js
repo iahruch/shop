790f770ea122b3692d5ea898a493d27b706ac86c
ihruch <ihruch.test@gmail.com>
2021-01-19 09:07:32 +0200

    fetch phone

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 
 import createRootReducer from 'reducers';
-import Layout from 'containers/layout';
+import {routes} from 'routes';
 
 const history = createBrowserHistory();
 const middlewares = [thunk, routerMiddleware(history)];
@@ -24,7 +24,7 @@ const store = createStore(
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Layout />
+      {routes}
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
